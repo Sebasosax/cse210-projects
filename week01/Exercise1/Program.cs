3d525171ab70c6264206196dd268a0e3118600af
Sebasosax <Sosa.sc@outlook.com>
2026-01-12 23:00:22 -0700

    week 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        Console.WriteLine("");
+
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("");
+        
+        Console.WriteLine("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine("");
+        Console.WriteLine($"Your  name is {lname}, {name} {lname} ");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage.");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        string sign = "";
+        int LastDigit = percentage % 10;
+        if (LastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (LastDigit < 3)
+        {
+            sign = "-";
+        }
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulation! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass this time. Keep trying!");
+        }
+
     }
+    
+
 }
