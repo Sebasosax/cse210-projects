a49e1294f4254f3dcc3c5def45929a2f1c064537
Sebasosax <Sosa.sc@outlook.com>
2026-02-18 01:06:02 -0700

    W7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace ExerciseTracking
+{
+    public abstract class Activity
+    {
+        private string _date;
+        private int _minutes;
+
+        public Activity(string date, int minutes)
+        {
+            _date = date;
+            _minutes = minutes;
+        }
+
+        public string Date { get { return _date; } }
+        public int Minutes { get { return _minutes; } }
+
+        public abstract double GetDistance();
+        public abstract double GetSpeed();
+        public abstract double GetPace();
+
+        public virtual string GetSummary()
+        {
+            return $"{_date} {this.GetType().Name} ({_minutes} min) - " +
+                   $"Distance {GetDistance():0.0} km, " +
+                   $"Speed {GetSpeed():0.0} kph, " +
+                   $"Pace: {GetPace():0.00} min per km";
+        }
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace ExerciseTracking
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        static void Main(string[] args)
+        {
+            var activities = new List<Activity>
+            {
+                new Running("03 Nov 2022", 30, 4.8),
+                new Cycling("03 Nov 2022", 45, 20),
+                new Swimming("03 Nov 2022", 60, 30)
+            };
+
+            foreach (var activity in activities)
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
     }
 }
